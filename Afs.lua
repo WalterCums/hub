aa99831a7b7d98678751c6fea79b754d002bdd02
WalterCums <73024559+WalterCums@users.noreply.github.com>
2020-12-29 00:47:09 +0100

    Update Afs.lua

--- Afs.lua
+++ Afs.lua
@@ -1,1 +1,1 @@
-game:GetService('Players').LocalPlayer:Kick('Buy Walter Hub for Anime fighting simulator. noob ;)');
+game:GetService('Players').LocalPlayer:Kick('Buy Walter Hub for for this game, walter hub is 3 dollars for the first 5 buyers buy now!!!');
